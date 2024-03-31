a7e99baf32ca267c520b8665a29495296cd7776a
Amadou Barro <barroama23@gmail.com>
2024-03-31 18:16:21 +0000

    Mettre en place un crud avec POO

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php 
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+ 
+
+
+<br>
+
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col_md-7"><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+                <div class="card shadow">
+                    <div class="card-header">
+                        <h1>Student Regitration form</h1>
+                    </div>
+                    <div class="card-body">
+                        <form action="">
+                            <label for="">Name</label>
+                            <input type="text" name="name" placeholder="entrer votre nom" class="form-control">
+
+                            <label for="">Email</label>
+                            <input type="email" name="email" placeholder="entrer votre email" class="form-control">
+ 
+                            <label for="">Phone</label>
+                            <input type="number" name="Phone" placeholder="entrer votre téléphone" class="form-control">
+
+                            
+                            <label for="">Photo</label>
+                            <input type="file" name="Photo" placeholder="entrer votre photo" class="form-control">
+
+    
+
+                            <label for="">Address</label>
+                          <textarea name="addresse" class="form-control"></textarea>
+                            <br>
+
+                          <input type="submit" value="register" class="btn btn-success form-control" >
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
